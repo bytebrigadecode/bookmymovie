80caaabfd42f0f5e71b8e1b5caea912dcb27beef
tempsms <tempsms21@gmail.com>
2023-05-18 13:58:14 +0530

    movie booking confirmation

--- src/components/BookMovie.js
+++ src/components/BookMovie.js
@@ -1,23 +1,107 @@
-import React from 'react'
+import React, { useState } from "react";
 
 import { useParams } from "react-router-dom";
-
+import DATA from "../MovieData.json";
+import "./style.css";
 export default function BookMovie() {
-    let {id}=useParams();
+  const [showModal, setShowModal] = useState(false);
+  const [selectedTime, setSelectedTime] = useState();
+
+  const [selectedTheater, setSelectedTheater] = useState();
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  
+
+  let { id } = useParams();
+
+  const movie = DATA.find((item) => item.id === parseInt(id));
+
+  const timeSlot = ["10:00AM", "2:00PM", "04:00PM", "06:00PM"];
+
+  const theaters = ["Theater1", "Theater2", "Theater3", "Theater4"];
+  const showModalHandle = () => {
+    setShowModal(true);
+  };
+
+  const confirmBooking = () => {
+    setShowConfirmation(true);
+    setShowModal(false)
+
+  };
   return (
     <div class="container">
-    <div class="row">
-      <div class="col-md-4">
-        <img style={{ height: "400px" }} src="https://m.media-amazon.com/images/M/MV5BZmMwOGFkYzgtODY0ZC00ZDJiLWE5MDUtNWFjNWEyZWMxMTM1XkEyXkFqcGdeQXVyMTMzNzIyNDc1._V1_.jpg" alt="Movie Poster" class="img-fluid"/>
+      <div class="row">
+        <div class="col-md-4">
+          <img
+            style={{ height: "400px" }}
+            src={movie.posterURL}
+            alt="Movie Poster"
+            class="img-fluid"
+          />
+        </div>
+        <div class="col-md-8">
+          <h2>{movie.name} </h2>
+          <p>Rating: {movie.rating}</p>
+          <p>Type: {movie.category}</p>
+          <p>Description: {movie.summary}</p>
+          <button onClick={showModalHandle} class="btn btn-primary">
+            Book Now
+          </button>
+        </div>
       </div>
-      <div class="col-md-8">
-        <h2>Movie Title {id} </h2>
-        <p>Rating: 1</p>
-        <p>Type: Action, Adventure, Sci-Fi</p>     
-        <p>Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum harum voluptatem tempore laboriosam iusto.</p>
-        <a href="#" class="btn btn-primary">Book Now</a>
-      </div>
+
+      {/* show modal */}
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <h3>Select Time and Theater</h3>
+            <form>
+              <label htmlFor="Time">Time</label>
+              <select
+                id="time"
+                value={selectedTime}
+                onChange={(e) => setSelectedTime(e.target.value)}
+              >
+                <option value="">Select Time</option>
+                {timeSlot.map((time, i) => (
+                  <option key={i} value={time}>
+                    {time}
+                  </option>
+                ))}
+              </select>
+
+              <label htmlFor="Time">Theater</label>
+              <select
+                id="time"
+                value={selectedTheater}
+                onChange={(e) => setSelectedTheater(e.target.value)}
+              >
+                <option value="">Select Theater</option>
+                {theaters.map((t, i) => (
+                  <option key={i} value={t}>
+                    {t}
+                  </option>
+                ))}
+              </select>
+              <button className="btn btn-primary" onClick={confirmBooking}>
+                confirm booking
+              </button>
+            </form>
+          </div>
+
+       
+        </div>
+      )}
+
+          {/* Confirmation page */}
+          {showConfirmation && (
+        <div className="confirmation-page">
+          <h2>Booking Confirmation</h2>
+          <p>Movie: {movie.name}</p>
+          <p>Time: {selectedTime}</p>
+          <p>Theater: {selectedTheater}</p>
+          <p>Enjoy the movie!</p>
+        </div>
+      )}
     </div>
-  </div>
-  )
+  );
 }
